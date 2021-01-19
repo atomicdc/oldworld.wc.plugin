12c118e863ccf8ddf7dd5dffed3acfcc194c81f9
Joe <joe@airbanana.com>
2021-01-19 13:46:36 +0000

    final clean up for prod

--- includes/data/data-settings.php
+++ includes/data/data-settings.php
@@ -22,13 +22,6 @@ return [
         'desc_tip' => true,
     ],
 
-    /*'packing' => [
-        'title' => __('Packages', 'woocommerce-shipping-freight'),
-        'type' => 'title',
-        'description' => __('The following settings determine how items are packed before being sent to freight.',
-            'woocommerce-shipping-freight'),
-    ],*/
-
     'packing_method' => [
         'title' => __('Packing Method', 'woocommerce-shipping-freight'),
         'type' => 'select',
@@ -39,33 +32,4 @@ return [
             /*'box_packing' => __('Pack into boxes with weights and dimensions (1:many)', 'woocommerce-shipping-freight'),*/
         ],
     ],
-
-    /*'boxes' => [
-        'type' => 'box_packing',
-    ],
-
-    'rates' => [
-        'title' => __('Rates and Services', 'woocommerce-shipping-freight'),
-        'type' => 'title',
-        'description' => __('The following settings determine the rates you offer your customers.',
-            'woocommerce-shipping-freight'),
-    ],
-
-    'residential' => [
-        'title' => __('Residential', 'woocommerce-shipping-freight'),
-        'label' => __('Default to residential delivery', 'woocommerce-shipping-freight'),
-        'type' => 'checkbox',
-        'disabled' => true,
-        'default' => 'yes',
-        /* // address validation is out of scope for now.
-         * 'description' => __('Enables residential flag. If you account has Address Validation enabled, this will be turned off/on automatically.', 'woocommerce-shipping-freight'),
-        'description' => __('Enables residential flag.', 'woocommerce-shipping-freight'),
-        'desc_tip' => true,
-        'class' => 'manuallyChecked'
-
-    ],*/
-
-    /*'services' => [
-        'type' => 'services',
-    ],*/
 ];
